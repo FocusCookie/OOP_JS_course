d0503ca9d5c1a4a1408f7728df06dca2338b80b0
FocusCookie <stephan_lueddemann@web.de>
2020-02-08 11:14:45 +0100

    Abstraction lection

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
 function Circle(radius) {
   this.radius = radius;
+
+  this.defaultLocation = { x: 0, y: 0 };
+
+  this.computeOptimumLocation = function() {
+    //... magic
+  };
+
   this.draw = function() {
+    this.computeOptimumLocation();
+
     console.log("draw a circle");
   };
 }
-
-const circle = new Circle(10);
-
-for (let key in circle) {
-  console.log(key);
-}
-// Output radius draw
-
-for (let key in circle) {
-  console.log(key, circle[key]);
-}
-// Output radius 10
-// draw f()....
-
-// only log the non functions
-for (let key in circle) {
-  if (typeof circle[key] !== "function") {
-    console.log(key, circle[key]);
-  }
-}
-
-const keys = Object.keys(circle);
-console.log(keys);
-
-if ("radius" in circle) console.log("Circle has a radius");
